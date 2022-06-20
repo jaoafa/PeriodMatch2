d6080771fd3c4fa220e82611f2cd5a3f1b6ec60b
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-06-20 23:57:19 +0900

    fix: インスペクションの適用

--- src/main/java/com/jaoafa/periodmatch2/PeriodMatchPlayer.java
+++ src/main/java/com/jaoafa/periodmatch2/PeriodMatchPlayer.java
@@ -11,147 +11,140 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class PeriodMatchPlayer {
-	public static final Map<UUID, PeriodMatchPlayer> players = new HashMap<>();
+    private static final Map<UUID, PeriodMatchPlayer> players = new HashMap<>();
 
-	final Player player; // プレイヤー
-	boolean perioding = false; // マッチ中か
-	boolean waiting = false;
-	int success = 0; // 成功回数
-	int failure = 0; // 失敗回数
-	int matchTime = -1; // マッチ時間
-	long startTime = -1; // 開始UNIXTime
-	BukkitTask mainTask = null;
+    private final Player player; // プレイヤー
+    private boolean perioding = false; // マッチ中か
+    private boolean waiting = false;
+    private int success = 0; // 成功回数
+    private int failure = 0; // 失敗回数
+    private int matchTime = -1; // マッチ時間
+    private long startTime = -1; // 開始UNIXTime
+    private BukkitTask mainTask = null;
 
-	public PeriodMatchPlayer(Player player) {
-		this.player = player;
-	}
+    private PeriodMatchPlayer(Player player) {
+        this.player = player;
+    }
 
-	/**
-	 * PeriodMatchをスタートさせます。
-	 */
-	public void start() {
-		perioding = true;
-		waiting = false;
+    public static PeriodMatchPlayer getPeriodMatchPlayer(Player player) {
+        if (PeriodMatchPlayer.players.containsKey(player.getUniqueId()))
+            return PeriodMatchPlayer.players.get(player.getUniqueId());
+        else {
+            PeriodMatchPlayer pmplayer = new PeriodMatchPlayer(player);
+            pmplayer.Save();
+            return pmplayer;
+        }
+    }
 
-		success = 0;
-		failure = 0;
+    /**
+     * PeriodMatchをスタートさせます。
+     */
+    public void start() {
+        perioding = true;
+        waiting = false;
 
-		startTime = System.currentTimeMillis();
+        success = 0;
+        failure = 0;
 
-		player.sendMessage("[PeriodMatch2] " + ChatColor.GREEN + "ピリオドマッチ計測を開始します。");
-		mainTask = new Task_MatchEnd(player, this).runTaskLater(Main.getJavaPlugin(), matchTime * 20L);
-		Save();
-	}
+        startTime = System.currentTimeMillis();
 
-	/**
-	 * PeriodMatchを強制終了させます。
-	 */
-	public void forceEnd() {
-		perioding = false;
-		waiting = false;
+        player.sendMessage("[PeriodMatch2] " + ChatColor.GREEN + "ピリオドマッチ計測を開始します。");
+        mainTask = new Task_MatchEnd(player, this).runTaskLater(Main.getJavaPlugin(), matchTime * 20L);
+        Save();
+    }
 
-		if (mainTask != null && !mainTask.isCancelled()) {
-			mainTask.cancel();
-		}
-		mainTask = null;
+    /**
+     * PeriodMatchを強制終了させます。
+     */
+    public void forceEnd() {
+        perioding = false;
+        waiting = false;
 
-		player.sendMessage("[PeriodMatch2] " + ChatColor.GREEN + "ピリオドマッチ計測を強制終了しました。");
-		clearTitle();
-		Save();
-	}
+        if (mainTask != null && !mainTask.isCancelled()) mainTask.cancel();
+        mainTask = null;
 
-	/**
-	 * PeriodMatchを終了させます。
-	 */
-	public void end() {
-		perioding = false;
-		mainTask = null;
+        player.sendMessage("[PeriodMatch2] " + ChatColor.GREEN + "ピリオドマッチ計測を強制終了しました。");
+        clearTitle();
+        Save();
+    }
 
-		player.sendMessage("[PeriodMatch2] " + ChatColor.GREEN + "ピリオドマッチ計測を終了しました。");
-		clearTitle();
-		Save();
-	}
+    /**
+     * PeriodMatchを終了させます。
+     */
+    public void end() {
+        perioding = false;
+        mainTask = null;
 
-	public void Success() {
-		if (!isPerioding()) {
-			return;
-		}
-		success++;
-		setTitle();
-		Save();
-	}
+        player.sendMessage("[PeriodMatch2] " + ChatColor.GREEN + "ピリオドマッチ計測を終了しました。");
+        clearTitle();
+        Save();
+    }
 
-	public void Failure() {
-		if (!isPerioding()) {
-			return;
-		}
-		failure++;
-		setTitle();
-		Save();
-	}
+    public void Success() {
+        if (!isPerioding()) return;
+        success++;
+        setTitle();
+        Save();
+    }
 
-	public void Save() {
-		players.put(player.getUniqueId(), this);
-	}
+    public void Failure() {
+        if (!isPerioding()) return;
+        failure++;
+        setTitle();
+        Save();
+    }
 
-	public void setTitle() {
+    public void Save() {
+        PeriodMatchPlayer.players.put(player.getUniqueId(), this);
+    }
+
+    private void setTitle() {
         player.sendTitlePart(TitlePart.TITLE, Component.text("PeriodMatch2", NamedTextColor.GOLD));
         player.sendTitlePart(TitlePart.SUBTITLE, Component.join(JoinConfiguration.noSeparators(),
             Component.text("成功: " + getSuccessCount(), NamedTextColor.GREEN),
             Component.text(" | "),
             Component.text(getFailureCount() + " :失敗", NamedTextColor.RED)));
-        player.sendTitlePart(TitlePart.TIMES, Title.Times.of(Duration.ZERO, Duration.ofSeconds(Integer.MAX_VALUE), Duration.ZERO));
-	}
+        player.sendTitlePart(TitlePart.TIMES, Title.Times.times(Duration.ZERO, Duration.ofSeconds(Integer.MAX_VALUE), Duration.ZERO));
+    }
 
-	public void clearTitle() {
-	    player.resetTitle();
-	}
+    private void clearTitle() {
+        player.resetTitle();
+    }
 
-	public boolean isPerioding() {
-		return perioding;
-	}
+    public boolean isPerioding() {
+        return perioding;
+    }
 
-	public boolean isWaiting() {
-		return waiting;
-	}
+    public boolean isWaiting() {
+        return waiting;
+    }
 
-	public void setWaiting(boolean waiting) {
-		this.waiting = waiting;
-	}
+    public void setWaiting(boolean waiting) {
+        this.waiting = waiting;
+    }
 
-	public int getSuccessCount() {
-		return success;
-	}
+    public int getSuccessCount() {
+        return success;
+    }
 
-	public int getFailureCount() {
-		return failure;
-	}
+    public int getFailureCount() {
+        return failure;
+    }
 
-	public int getMatchTime() {
-		return matchTime;
-	}
+    public int getMatchTime() {
+        return matchTime;
+    }
 
-	public long getStartTime() {
-		return startTime;
-	}
+    public void setMatchTime(int matchTime) {
+        this.matchTime = matchTime;
+    }
 
-	public void setMatchTime(int matchTime) {
-		this.matchTime = matchTime;
-	}
-
-	public static PeriodMatchPlayer getPeriodMatchPlayer(Player player) {
-		if (players.containsKey(player.getUniqueId())) {
-			return players.get(player.getUniqueId());
-		} else {
-			PeriodMatchPlayer pmplayer = new PeriodMatchPlayer(player);
-			pmplayer.Save();
-			return pmplayer;
-		}
-	}
+    public long getStartTime() {
+        return startTime;
+    }
 }
